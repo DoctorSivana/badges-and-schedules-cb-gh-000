3a5599e533374f7fc8cd9fb0a924cacccd269763
Adil Abed <dr.sivana@protonmail.com>
2017-11-17 20:56:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,19 @@
 # Write your code here.
 def badge_maker(names)
-  puts "Hello, my names is #{names}"
+  puts "Hello, my names is #{names}."
 end
+
+
 def batch_badge_creator(array)
   array.each do |elements|
     badge_maker(elements)
-  
+  end
 end
-end
+
+
 def assign_rooms(array)
   array.each_with_index do |components , index|
     puts "Hello, #{components} ! You'll be assigned to room #{index}!"
-  
+  end
 end
+
